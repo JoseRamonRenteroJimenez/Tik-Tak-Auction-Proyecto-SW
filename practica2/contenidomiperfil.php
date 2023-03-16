1f9b2a0a84e9271e009925f5abbe33f2af795985
albertorn96 <albertodelarubia96@gmail.com>
2023-03-16 17:37:16 +0100

    muestra select en perfil usuario

--- practica2/contenidomiperfil.php
+++ practica2/contenidomiperfil.php
@@ -1,32 +1,38 @@
 <?php
 require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/includes/src/subastas/listadoSubastas.php';
 
 $tituloPagina = 'Mi perfil';
 $contenidoPrincipal='';
 
 if ($app->tieneRol(es\ucm\fdi\aw\usuarios\Usuario::USER_ROLE)||$app->tieneRol(es\ucm\fdi\aw\usuarios\Usuario::BUSSINES_ROLE)) {
-    $formLogin = new \es\ucm\fdi\aw\usuarios\FormularioLogin();
-    $formLogin = $formLogin->gestiona();
     
     $addsubastaUrl = $app->resuelve('/addSubasta.php');
-       
+    $mensajes = $app->resuelve('/chat.php');
+    $actividad = $app->resuelve('/actividadPerfil.php');
+    $notificaciones = $app->resuelve('/listaNotificaciones.php');
+    $listarsubastas = new \es\ucm\fdi\aw\subastas\listadoSubastas();
+    $listarsubastas = $listarsubastas->gestiona();   
 
   $contenidoPrincipal=<<<EOS
-    <h1>Vista general de subastas en Mi tiktak </h1>
-    <div>
-    <a href="url">Actividad</a>
-    <a href="url">Mensajes</a>
-    <a href="url">Noticias</a>
-   
-    </div>
-    <a href="{$addsubastaUrl}">subir subasta</a>
+
+                            <div>
+                          
+                            <a href="{$addsubastaUrl}">subir subasta</a>
+                            $listarsubastas
+                            </div>
+
+    
   EOS;
+  $contenidoPrincipal .= \es\ucm\fdi\aw\subastas\listasubastas();
+ 
 } else {
   $contenidoPrincipal=<<<EOS
     <h1>Usuario no registrado!</h1>
     <p>Debes iniciar sesión para ver el contenido.</p>
+    
   EOS;
 }
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
-$app->generaVista('/plantillas/plantilla.php', $params);
+$app->generaVista('/plantillas/plantillaPerfil.php', $params);
---
+++ practica2/includes/src/subastas/listadoSubastas.php
@@ -0,0 +1,133 @@
+<?php
+namespace es\ucm\fdi\aw\subastas;
+
+use es\ucm\fdi\aw\Aplicacion;
+use es\ucm\fdi\aw\Formulario;
+
+class ListadoSubastas extends Formulario
+{
+    public function __construct() {
+        parent::__construct('formObjeto', ['urlRedireccion' => Aplicacion::getInstance()->resuelve('/index.php')]);
+    }
+    
+    protected function generaCamposFormulario(&$datos)
+    {
+       
+        $tipo = $datos['TipoSubasta'] ?? '';
+        $estado = $datos['Estado'] ?? '';
+        $relevancia = $datos['Relevancia'] ?? '';
+
+        // Se generan los mensajes de error si existen.
+        $htmlErroresGlobales = self::generaListaErroresGlobales($this->errores);
+        $erroresCampos = self::generaErroresCampos(['TipoSubasta', 'Estado', 'Relevancia'], $this->errores, 'span', array('class' => 'error'));
+
+        $html = <<<EOF
+        $htmlErroresGlobales
+        //Creamos aqui la parte fija del codigo HTML
+        <form method="post">
+            <p>Todas</p>
+            <select name="TipoSubasta" value="$tipo">
+                <option>Tipo de subasta</option>
+                <optgroup>
+                    <option>opcion1</option>
+                </optgroup>
+            </select>
+            $erroresCampos[TipoSubasta]
+
+            <select name="Estado" value="$estado">
+                <option>Estado</option>
+                <optgroup>
+                    <option>opcion1</option>
+                </optgroup>
+            </select>
+            $erroresCampos[Estado]
+            <p>Ordenar</p>
+            <select name="Relevancia" value="$relevancia">
+                <option>Relevancia</option>
+                <optgroup>
+                    <option>opcion1</option>
+                </optgroup>
+            </select>
+            $erroresCampos[Relevancia]
+        </form>
+        EOF;
+        return $html;
+    }
+    
+
+    protected function procesaFormulario(&$datos)
+    {
+       
+        $tipo = $datos['TipoSubasta'] ?? '';
+        $estado = $datos['Estado'] ?? '';
+        $relevancia = $datos['Relevancia'] ?? '';
+
+
+
+        $app = Aplicacion::getInstance();
+        $idUsuario = $app->idUsuario();
+        
+      
+        $subastas = array(); // Declarar $subastas como un array vacío
+        $resultados = Subasta::buscaSubasta("");
+        echo ($resultados.getEstado());
+        // Agregar cada objeto Subasta devuelto por el método al array $subastas
+        foreach ($resultados as $fila) {
+            $subasta = new Subasta($fila['id_usuario'], $fila['titulo'], $fila['descripcion'], $fila['fecha_inicio'], $fila['fecha_fin'], $fila['precio_inicial'], $fila['precio_actual'], $fila['imagen'], $fila['categoria'], $fila['estadoproducto'], $fila['id_subasta'], $fila['id_ganador'], $fila['estado']);
+            array_push($subastas, $subasta); // o también: $subastas[] = $subasta;
+        }
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Titulo</th><th>Descripcion</th><th>Fecha de inicio</th><th>Fecha de fin</th><th>Precio inicial</th><th>Precio actual</th><th>ID ganador</th><th>Estado</th><th>Imagen</th><th>Categoria</th></tr>";
+        foreach ($subastas as $subasta) {
+            echo "<tr>";
+            echo "<td>" . $subasta->getIdSubasta() . "</td>";
+            echo "<td>" . $subasta->getTitulo() . "</td>";
+            echo "<td>" . $subasta->getDescripcion() . "</td>";
+            echo "<td>" . $subasta->getFechaInicio() . "</td>";
+            echo "<td>" . $subasta->getFechaFin() . "</td>";
+            echo "<td>" . $subasta->getPrecioInicial() . "</td>";
+            echo "<td>" . $subasta->getPrecioActual() . "</td>";
+            echo "<td>" . $subasta->getIdGanador() . "</td>";
+            echo "<td>" . $subasta->getEstado() . "</td>";
+            echo "<td>" . $subasta->getImagen() . "</td>";
+            echo "<td>" . $subasta->getCategoria() . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";      
+    }
+}
+
+function listasubastas()
+{
+    $subasta = Subasta::buscaSubasta("e");
+    /*if (count($subasta) == 0) {
+        return '';
+    }*/
+
+    $html = '<ul>';
+   //foreach($subasta as $subasta) {
+        $html .= '<li>';
+        $html .= visualizaSubasta($subasta);
+      // echo($subasta);
+        
+        $html .= '</li>';
+  // }
+    $html .= '</ul>';
+
+    return $html;
+}
+function visualizaSubasta($subasta)
+{
+    /*$verURL = Utils::buildUrl('mensajes/mensajes.php', [
+        'id' => $subasta->id
+    ]);*/
+    //echo($subasta);
+    // echo($subasta->id_usuario.",".$subasta->titulo .",".$subasta->descripcion .",".$subasta->fecha_inicio.",". $subasta->fecha_fin .",".$subasta->precio_inicial.",". $subasta->precio_actual.",". $subasta->id_ganador .",".$subasta->estado .",".$subasta->imagen .",".$subasta->categoria.",". $subasta->estadoproducto.",". $subasta->obtenerEstadoSubasta($subasta->fecha_inicio,$subasta->fecha_fin));
+
+    
+    
+    return <<<EOS
+            <p>$subasta->descripcion $subasta->titulo </p>
+    EOS;
+}
+?>
